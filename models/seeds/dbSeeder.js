3b6ad6896ea55573fa6dcea77a28ead74fda75cc
Eric Huang <86519473+erikku54@users.noreply.github.com>
2023-07-22 15:31:47 +0800

    feat: revise seed data and scripts

---
+++ models/seeds/dbSeeder.js
@@ -0,0 +1,29 @@
+
+// environment setting 僅在非正式環境時使用dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ path: './.env' });
+}
+
+const db = require('../../config/mongoose'); // 取得連線
+
+const userModel = require('../userModel');
+const restaurantModel = require('../restaurantModel');
+const users = require('./seeds.json').users;
+const restaurants = require('./seeds.json').restaurants;
+
+
+db.once('open', () => {
+
+  Promise.all([userModel.create(users), restaurantModel.create(restaurants)])
+    .then(() => {
+      console.log('Database seed data was created successfully.')
+      return Promise.all([userModel.findOne({ name: 'User1' }), userModel.findOne({ name: 'User2' })]);
+    })
+    .then(users => Promise.all([restaurantModel.updateMany({ id: { $in: [1, 2, 3] } }, { userId: users[0]._id }),
+      restaurantModel.updateMany({ id: { $in: [4, 5, 6] } }, { userId: users[1]._id })]))
+    .then(() => {
+      console.log('User-Restaurant relation was set successfully.');
+      process.exit();
+    })
+    .catch(err => console.log('mongodb error:', err));
+})
